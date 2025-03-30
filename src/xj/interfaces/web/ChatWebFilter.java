7be879832c25cfa69a0243799fdca608b473bc45
Xiaojiu233 <RT_200@163.com>
2025-03-30 19:04:14 +0800

    添加专用Filter

---
+++ src/xj/interfaces/web/ChatWebFilter.java
@@ -0,0 +1,10 @@
+package xj.interfaces.web;
+
+import xj.abstracts.connect.ConnectHandler;
+
+/**
+ * TCP聊天室协议专用过滤器
+ */
+public interface ChatWebFilter extends WebFilter {
+
+}
---
+++ src/xj/interfaces/web/HttpWebFilter.java
@@ -0,0 +1,10 @@
+package xj.interfaces.web;
+
+import xj.abstracts.connect.ConnectHandler;
+
+/**
+ * HTTP协议专用过滤器
+ */
+public interface HttpWebFilter extends WebFilter {
+
+}
